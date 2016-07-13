6e570469df31c00da69bcbdf8cc04b66ef297bf3
Mukesh_50 <Mukesh_50@192.168.56.1>
2016-07-13 21:58:16 +0530

    Signed-off-by: Mukesh_50 <Mukesh_50@192.168.56.1>

--- src/test/java/Pages/SalesforceContactPage.java
+++ src/test/java/Pages/SalesforceContactPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.relevantcodes.extentreports.LogStatus;
@@ -29,6 +30,12 @@ public class SalesforceContactPage extends BaseClass
 	
 	@FindBy(name="save") WebElement saveButton;
 	
+	@FindBy(xpath="//*[text()='Lead Source']//following::select[1]")WebElement leadScore;
+	
+	@FindBy(xpath="//*[text()='Lead Source']//following::select[2]")WebElement level;
+	
+	@FindBy(xpath="//*[text()='Description']//following::textarea") WebElement description;
+	
 	
 	
 	public void createContacts()
@@ -40,7 +47,17 @@ public class SalesforceContactPage extends BaseClass
 		Helper.highLightElement(driver, newButton).click();
 		
 		Helper.highLightElement(driver, lastName).sendKeys("Selenium 1");
-		
+	
+	    Helper.highLightElement(driver, leadScore);
+	    
+	    new Select(leadScore).selectByIndex(2);
+	    
+         Helper.highLightElement(driver, level);
+	    
+	    new Select(level).selectByIndex(2);
+	    
+	    Helper.highLightElement(driver, description).sendKeys("Kaplan contact creation");
+	    
 		Helper.highLightElement(driver, saveButton).click();
 		
 		logger.log(LogStatus.INFO, "Contact created");
